e8d5d79da9a484dee888457da5de06e783987551
Andre Silva <andre.silva@movile.com>
2015-12-31 20:34:48 -0200

    Inicial Commit

---
+++ Example/Pods/Target Support Files/Pods-CurrencyTextField_Example/Pods-CurrencyTextField_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_CurrencyTextField_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CurrencyTextField_ExampleVersionString[];
+
